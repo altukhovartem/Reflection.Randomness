bcb91a8c154f1d40c45a6c21f0cdcc139530a963
Artem <altukhov.artem.dev@gmain.com>
2018-05-23 22:47:12 +0300

    in progress

--- Reflection.Randomness.csproj/Generator.cs
+++ Reflection.Randomness.csproj/Generator.cs
@@ -6,5 +6,50 @@ using System.Threading.Tasks;
 
 namespace Reflection.Randomness
 {
-    
+	[AttributeUsage(AttributeTargets.Property)]
+    class FromDistribution : Attribute
+	{
+
+		#region Props
+
+		public Type TypeOfDistribution { get; set; }
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int Z { get; set; }
+
+		#endregion
+
+		#region Ctors
+
+		public FromDistribution(Type typeOfDistribution)
+		{
+			this.TypeOfDistribution = typeOfDistribution;
+		}
+
+		public FromDistribution(Type typeOfDistribution, int X)
+			: this(typeOfDistribution)
+		{
+			this.X = X;
+		}
+
+		public FromDistribution(Type typeOfDistribution, int X, int Y)
+			: this(typeOfDistribution, X)
+		{
+			this.Y = Y;
+		}
+
+		public FromDistribution(Type typeOfDistribution, int X, int Y, int Z)
+			: this(typeOfDistribution, X, Y)
+		{
+			this.Z = Z;
+		}
+
+		#endregion
+
+	}
+
+	class Generator<T1>
+	{
+		public void 
+	}
 }
--- Reflection.Randomness.csproj/Program.cs
+++ Reflection.Randomness.csproj/Program.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using NUnitLite;
+using Reflection.Randomness;
 
 class Program
 {
 	static void Main(string[] args)
-	{
+	{ 
 		new AutoRun().Execute(args);
 	}
 }
