2d1e12fb328b063c24e015351be04320e6a36b7d
Artem <altukhov.artem.dev@gmain.com>
2018-06-06 23:00:22 +0300

    in progress

--- Reflection.Randomness.csproj/Generator.cs
+++ Reflection.Randomness.csproj/Generator.cs
@@ -32,15 +32,11 @@ namespace Reflection.Randomness
 		static Dictionary<PropertyInfo, IContinousDistribution> staticDictionary = new Dictionary<PropertyInfo, IContinousDistribution>();
 		public Dictionary<PropertyInfo, IContinousDistribution> dynamicDictionary = new Dictionary<PropertyInfo, IContinousDistribution>();
 
-		public Generator()
-		{
-
-		}
+		public Generator(){}
 
 		static Generator()
 		{
-			Type typeOfCurrentClass = typeof(T);
-			PropertyInfo[] collectionOfProperties = typeOfCurrentClass.GetProperties();
+			PropertyInfo[] collectionOfProperties = typeof(T).GetProperties();
 			foreach (var prop in collectionOfProperties)
 			{
 				FromDistribution attribute = Attribute.GetCustomAttribute(prop, typeof(FromDistribution)) as FromDistribution;
@@ -71,24 +67,7 @@ namespace Reflection.Randomness
 
 			return generatorInstance as T;
 		}
-
-
-		//public T Generate(Random random)
-		//{
-		//	object generatorInstance = Activator.CreateInstance(typeof(T));
-		//	var props = typeof(T).GetProperties().Where(p => Attribute.IsDefined(p, typeof(FromDistribution)));
-		//	foreach (var prop in props)
-		//	{
-		//		FromDistribution attribute = (FromDistribution)Attribute.GetCustomAttribute(prop, typeof(FromDistribution));
-		//		var valueOfDistribution = attribute.GetDistributionValue(random);
-		//		prop.SetValue(generatorInstance, valueOfDistribution);
-		//	}
-
-		//	return generatorInstance as T;
-		//}
-
 	
-
 		public ISettable<T> For(Expression<Func<T, object>> p)
 		{
 			Type type = p.GetType();
@@ -122,13 +101,6 @@ namespace Reflection.Randomness
 
 		public Generator<T> Set(IContinousDistribution distribution)
 		{
-			//у тебя есть два способа сообщить генератору о том какие распределения проассоциированы с какими полями
-			//1. либо непосредственно на само поле повесить атрибут
-			//2. либо лично генератору подсказать
-
-			//1: первый способ хорош если этот класс на поле которого ты вешаешь в твоем распоряжении и ты можешь на его поля повесить атрибут
-			//2: второй способ хорош когда ты не можешь повесить на поле атрибут, потому что это не твой класс, а класс из какой то другой библиотеки
-
 			currentGenerator.dynamicDictionary.Add(classProperty, distribution);
 			return currentGenerator;
 		}
